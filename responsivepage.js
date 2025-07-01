55842812282cab73d2fc49b0f93077c8f64c13cd
mayankP1409 <mayankpandya2004@gmail.com>
2025-07-02 00:36:10 +0530

    first

---
+++ responsivepage.js
@@ -0,0 +1,10 @@
+ window.onload = function () {
+      const navbar = document.getElementById('navbar');
+      window.addEventListener('scroll', () => {
+        if (window.scrollY > 60) {
+          navbar.style.boxShadow = "0 2px 10px rgba(0,0,0,0.1)";
+        } else {
+          navbar.style.boxShadow = "none";
+        }
+      });
+    };
